298665c9e70c59bda7a6cec7cf7f2b0bf23fe731
FloMmacaron1 <florent.liandier@gmail.com>
2025-11-05 19:26:00 +0100

    Create CodeGPS.ino

---
+++ Nereides-2026/Codes_tests/CodeGPS/CodeGPS.ino
@@ -0,0 +1,23 @@
+#include <TinyGPS++.h>
+#include <HardwareSerial.h>
+
+TinyGPSPlus gps;
+HardwareSerial SerialGPS(1); // Utilise le port série 1 de l’ESP32
+
+void setup() {
+  Serial.begin(115200);
+  SerialGPS.begin(9600, SERIAL_8N1, 16, 17); // RX=16, TX=17
+  Serial.println("Lecture GPS en cours...");
+}
+
+void loop() {
+  while (SerialGPS.available() > 0) {
+    gps.encode(SerialGPS.read());
+  }
+
+  if (gps.location.isUpdated()) {
+    Serial.print("Latitude : "); Serial.println(gps.location.lat(), 6);
+    Serial.print("Longitude : "); Serial.println(gps.location.lng(), 6);
+    Serial.print("Satellites : "); Serial.println(gps.satellites.value());
+  }
+}
